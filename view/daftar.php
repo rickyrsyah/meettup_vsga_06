1d5fd489d7ed1cd6e2da2d76c137b321e501d0da
Ricky R. Syahputra <rikysyahputra21@hotmail.com>
2019-07-08 15:34:50 +0700

    belajar struktur web, tabel dan php

---
+++ view/daftar.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Daftar Peserta</title>
+	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+	<script src="../assets/js/jquery.slim.min.js"></script>
+	<script src="../assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light"><h3>DIGITAL TALENT | Form using Bootstrap</h3></nav>
+	<div class="container">
+		<table class="table table-bordered" style="margin-top: 40px">
+			<tr>
+				<th>No.</th>
+				<th>Nama</th>
+				<th>E-mail</th>
+				<th>Alamat</th>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>Budi</td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Andi</td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>Susi</td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>Santi</td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>Edi</td>
+				<td></td>
+				<td></td>
+			</tr>
+		</table></div>
+</body>
+</html>
